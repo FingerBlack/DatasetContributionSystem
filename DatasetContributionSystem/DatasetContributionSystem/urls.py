dd1af753085f55986c760c5f7d53b2765eff128d
任子尧 <2571476073@qq.com>
2019-11-08 15:01:52 +0800

    import bootstrap, jquery, create templates base.html

--- DatasetContributionSystem/DatasetContributionSystem/urls.py
+++ DatasetContributionSystem/DatasetContributionSystem/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from homepage import views as homepage_views
 
 urlpatterns = [
+    path('', homepage_views.index),
     path('admin/', admin.site.urls),
 ]
