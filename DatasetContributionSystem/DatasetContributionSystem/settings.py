5e82d57b781c2d81a772b42584e7ba24de479a40
任子尧 <2571476073@qq.com>
2019-11-12 12:38:13 +0800

    create database dataset

--- DatasetContributionSystem/DatasetContributionSystem/settings.py
+++ DatasetContributionSystem/DatasetContributionSystem/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'homepage',
+    'dataset', 
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'DatasetContributionSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbhomework',
+        'PASSWORD': 'password', 
+        'HOST': '127.0.0.1', 
+        'PORT':5432
     }
 }
 
@@ -119,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#import local_settings
+try:
+    from local_settings import *
+except ImportError:
+    pass
