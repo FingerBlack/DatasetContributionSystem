bbc2b21efa3e18c23f54e547fd6f2f0b376886b4
ZhuZhengbang <zhu990729@163.com>
2019-11-13 21:12:47 +0800

    merge

--- DatasetContributionSystem/DatasetContributionSystem/settings.py
+++ DatasetContributionSystem/DatasetContributionSystem/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'homepage',
     'user', 
-    'dataset', 
-    'task',
+    'dataset',
 ]
 
 MIDDLEWARE = [
@@ -81,8 +80,8 @@ WSGI_APPLICATION = 'DatasetContributionSystem.wsgi.application'
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'dbhomework',
-#         'PASSWORD': 'password', 
-#         'HOST': '127.0.0.1', 
+#         'PASSWORD': 'password',
+#         'HOST': '127.0.0.1',
 #         'PORT':5432
 #     }
 # }
@@ -90,6 +89,12 @@ WSGI_APPLICATION = 'DatasetContributionSystem.wsgi.application'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+
+#指定用户认证表
+AUTH_USER_MODEL = "user.UserProfile"
+
+#指定登陆url
+LOGIN_URL = '/login/'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -125,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = '/upload/'
 
 #import local_settings
 try:
