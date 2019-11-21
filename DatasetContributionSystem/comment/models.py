b6b2ee9b724dbbe143f2c3277ac9cc7c711df892
Zfancy <814168136@qq.com>
2019-11-21 11:15:31 +0800

    未作评论展示

--- DatasetContributionSystem/comment/models.py
+++ DatasetContributionSystem/comment/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+import django.utils.timezone as timezone
 
 # Create your models here.
 class comment(models.Model):
     DatasetName = models.CharField(max_length = 50, primary_key = True)
     Username = models.CharField(max_length = 50)
-    Description = models.IntegerField()
+    Description = models.CharField(max_length = 50)
     Score = models.BooleanField()
     Time = models.DateTimeField()
--- DatasetContributionSystem/comment/views.py
+++ DatasetContributionSystem/comment/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-def idex(request,datasetname):
-
-    return render(request,'comment/comment.html')
+from dataset.models import dataset
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.conf import settings
 
 def post(request,datasetname):
+    '''
+    if request.method == "POST":
+        name = request.POST.get("name", '')
+        description = request.POST.get('description', '')
+        dataset.objects.create(DatasetName = ,
+                               Username = ,
+                               Description = ,
+                               Score = ,
+                               Time = )
+        return HttpResponseRedirect('/comment/'+name+'/')
+
+    '''
+
     return render(request,'comment/post.html')
+
+def idex(request,datasetname):
+    return render(request,'comment/comment.html')
 # Create your views here.
