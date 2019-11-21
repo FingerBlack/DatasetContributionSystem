43ee90b46b61c7afb695226524488417985ec79a
任子尧 <2571476073@qq.com>
2019-11-21 16:39:10 +0800

    增加icon

--- DatasetContributionSystem/DatasetContributionSystem/urls.py
+++ DatasetContributionSystem/DatasetContributionSystem/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.views import serve
 from django.urls import path
 from homepage import views as homepage_views
 from user import views as user_views
