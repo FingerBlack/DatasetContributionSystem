6b3fe42155543c5d6cd222928e38878f0b5ef64e
任子尧 <2571476073@qq.com>
2019-11-25 00:27:40 +0800

    rm .DS_Store 修改上传流程 在上传后显示上传文件(alert)

--- DatasetContributionSystem/dataset/views.py
+++ DatasetContributionSystem/dataset/views.py
@@ -3,7 +3,7 @@ from .models import dataset
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.conf import settings
-import os, zipfile, shutil, uuid
+import os, zipfile, shutil, uuid, json
 
 # Create your views here.
 def index(request):
@@ -28,36 +28,46 @@ def create(request):
 class Upload():
     #define the relation between function and type
     def image_recognition(self):
-        myfile = self.file
-        zf = zipfile.ZipFile(myfile)
-        filename = myfile.name
-        filepath = os.path.join('./', settings.MEDIA_ROOT , '/tmp/', myfile.filename)
-        zf.extractall(path=filepath)
-        pic_list = os.listdir(filepath+'/'+os.listdir(filepath)[0]+'/')
-        src = filepath+'/'+os.listdir(filepath)[0]+'/'
-        dest = os.path.join("./"+settings.MEDIA_ROOT, 'dataset', self.dataset.name)
-        if os.path.exists(dest) == False:
-            os.mkdir(dest)
-        dataset_len = 0
-        for item in pic_list:
-            tmp = item.split('.')
-            pic_pre = '.'.join(tmp[:-1])
-            if tmp[-1] == 'jpg' and os.path.exists(src + '/' + pic_pre + '.txt'):
-               dataset_len += 1
-               randomname = str(uuid.uuid4())
-               shutil.move(src+'/'+ pic_pre + '.txt', dest + '/'+ randomname + '.txt')
-               shutil.move(src+'/'+ pic_pre + '.jpg', dest + '/'+ randomname + '.jpg')
-        self.dataset.size += dataset_len
+        ret = {}
+        zf = None
+        try:
+            zf = zipfile.ZipFile(self.file)
+            zf.testzip()
+        except:
+            ret['status'] = 'error'
+            ret['message'] = 'bad zip file'
+            return ret
+        dir_dest = os.path.join('.' + settings.MEDIA_ROOT, 'dataset', self.dataset.name)
+        #check whether the dataset is empty
+        if os.path.exists(dir_dest) == False:
+            os.mkdir(dir_dest)
+        ret['status'] = 'ok'
+        ret['acceptFileList'] = []
+        for item in zf.infolist():
+            fn_jpg = item.filename.split('.')
+            if fn_jpg[-1] != 'jpg':
+                #ignore the file
+                continue
+            fn_txt = fn_jpg.copy()
+            fn_txt[-1] = 'txt'
+            if '.'.join(fn_txt) not in zf.namelist():
+                #ignore the file which has no tag
+                continue
+            ret['acceptFileList'].append('.'.join(fn_jpg))
+            targetName = str(uuid.uuid4())
+            with open(os.path.join(dir_dest, targetName + '.jpg'), 'wb') as f:
+                f.write(zf.read('.'.join(fn_jpg)))
+            with open(os.path.join(dir_dest, targetName + '.txt'), 'wb') as f:
+                f.write(zf.read('.'.join(fn_txt)))
+            self.dataset.size += 1
         self.dataset.save()
-        if os.path.exists(filepath) == True:
-            shutil.rmtree(filepath)
-        return HttpResponse('success')
+        return ret
     type_to_func = {2: image_recognition, 1: image_recognition}
     def __init__(self, file, dataset):
         self.file = file
         self.dataset = dataset
     def handle(self):
-        return self.type_to_func[self.dataset.dataType](self)
+        return HttpResponse(json.dumps(self.type_to_func[self.dataset.dataType](self)))
 
 @login_required
 def upload_view(request, datasetname):
