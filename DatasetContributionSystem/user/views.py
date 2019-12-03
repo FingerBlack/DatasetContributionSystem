7344fce37fcadd3819acad87883b434f9327ad1d
cxhZCM123 <931736813@qq.com>
2019-12-03 13:30:33 +0800

    upload

--- DatasetContributionSystem/user/views.py
+++ DatasetContributionSystem/user/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password, check_password
 import os, uuid
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from PIL import Image
 
 # Create your views here.
@@ -77,7 +77,14 @@ def revise_view(request, username):
     return render(request, 'user/revise.html') 
 
 @login_required
-def avator_view(request, username):
+def show_avatar(request, username):
+    img_path = '.' + request.user.avatar
+    file = open(img_path, "rb")
+    response = FileResponse(file)
+    return response
+
+@login_required
+def avatar_view(request, username):
     if request.method == 'POST':
         img = request.FILES.get('img')
         try:
@@ -86,12 +93,12 @@ def avator_view(request, username):
             return render(request, 'failure.html', {'title':'请检查上传图片格式'})
 
         img_name = str(uuid.uuid4())
-        img_path = os.path.join('.' + settings.MEDIA_ROOT, 'avator')
+        img_path = os.path.join('.' + settings.MEDIA_ROOT, 'avatar')
         if not os.path.exists(img_path):
             os.makedirs(img_path)
         img.save(os.path.join(img_path, img_name + '.jpg'))
-        request.user.avator = os.path.join(settings.MEDIA_ROOT, 'avator', img_name + '.jpg')
+        request.user.avatar = os.path.join(settings.MEDIA_ROOT, 'avatar', img_name + '.jpg')
         request.user.save()
         return render(request, 'success.html', {'title':'修改头像成功'})
             
-    return render(request, 'user/avator.html') 
+    return render(request, 'user/avatar.html') 
