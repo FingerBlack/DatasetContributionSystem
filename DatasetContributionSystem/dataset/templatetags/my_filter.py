519664eb4f354a35622101d5e9c9c60d70bd54b1
任子尧 <2571476073@qq.com>
2019-12-05 08:46:45 +0800

    修复unitConvert<1024时仍显示小数的bug

--- DatasetContributionSystem/dataset/templatetags/my_filter.py
+++ DatasetContributionSystem/dataset/templatetags/my_filter.py
@@ -4,10 +4,12 @@ register = template.Library()
 
 @register.filter
 def unitConvert(num):
-    unit = ['', 'K', 'M', 'G', 'T', 'E']
+    unit = ['K', 'M', 'G', 'T', 'E']
+    if num < 1024:
+        return str(num)
     num = float(num)
     for i in range(0, 5):
+        num /= 1024
         if num < 1024:
             return str(round(num, 2)) + unit[i]
-        num /= 1024
-    return str(round(num, 2)) + 'E'
+    return str(round(num, 2)) + unit[i]
